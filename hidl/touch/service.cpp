whoisgabutuniverseshit/hardware_transsion

hidl: Implement vendor.lineage.touch@1.0 HAL for Transsion
* This can be used by any Transsion device that has a touch display that supports multiple sample rates
* Most newer Transsion phones with high screen refresh rates fit this category:
* e.g. Note 30/Pro, Transsion MT6789 platform phones & Transsion MT6893 phones

Change-Id: Iaf956420157bbc978690fac9053d507ccaea847a
Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

hidl/touch/service.cpp
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "lineage.touch@1.0-service.xiaomi"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "HighTouchPollingRate.h"
+
+using ::vendor::lineage::touch::V1_0::IHighTouchPollingRate;
+using ::vendor::lineage::touch::V1_0::implementation::HighTouchPollingRate;
+
+int main() {
+    android::sp<IHighTouchPollingRate> highTouchPollingRate = new HighTouchPollingRate();
+
+    android::hardware::configureRpcThreadpool(1, true);
+
+    if (highTouchPollingRate->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register touchscreen high polling rate HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touchscreen HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
+    return 1;
+}

roms-experimental/android_hardware_transsion

hidl/touch/service.cpp
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "lineage.touch@1.0-service.transsion"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "HighTouchPollingRate.h"
+
+using ::vendor::lineage::touch::V1_0::IHighTouchPollingRate;
+using ::vendor::lineage::touch::V1_0::implementation::HighTouchPollingRate;
+
+int main() {
+    android::sp<IHighTouchPollingRate> highTouchPollingRate = new HighTouchPollingRate();
+
+    android::hardware::configureRpcThreadpool(1, true);
+
+    if (highTouchPollingRate->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register touchscreen high polling rate HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touchscreen HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
+    return 1;
+}