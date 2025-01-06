2b97f9b66fc92bedfc9f2d0458578722c75dcec0
dodyirawan85 <dev.irawans@gmail.com>
2025-01-06 07:20:20 +0700

    sepolicy: Label camera properties
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -1,1 +1,3 @@
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/transsion/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += hardware/transsion/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += hardware/transsion/sepolicy/public
